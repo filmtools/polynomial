917066d8f1274548c47c222f652dda26dd0a21a3
Carsten Witt <412560+tomkyle@users.noreply.github.com>
2019-04-26 11:52:31 +0200

    Add some comments

--- src/DerivativeCoefficientsProvider.php
+++ src/DerivativeCoefficientsProvider.php
@@ -24,12 +24,15 @@ class DerivativeCoefficientsProvider implements CoefficientsProviderInterface
         $coefficients = $this->coefficients_provider->getCoefficients();
         $coefficients = iterable_to_array( $coefficients );
 
+        // In derivatives, the last coefficient is ignored
         if (array_key_exists(0, $coefficients))
             unset($coefficients[0]);
 
+        // Hmmm. Should it be empty?
         if (count($coefficients) === 0)
             return new \SplFixedArray( 0 );
 
+        // Perform the derivation
         $new_coefficients = array(0);
         foreach($coefficients as $exponent => $factor)
             $new_coefficients[ $exponent - 1 ] = $exponent * $factor;
--- src/XFinderInterface.php
+++ src/XFinderInterface.php
@@ -3,5 +3,14 @@ namespace FilmTools\PolynomialModel;
 
 interface XFinderInterface
 {
+
+    /**
+     * Finds an X for a given Y coordinate.
+     *
+     * @param  float $y Y value
+     * @return float $x Found X coordinate
+     *
+     * @throws XNotFoundException
+     */
     public function findX( float $y ) : float;
 }
